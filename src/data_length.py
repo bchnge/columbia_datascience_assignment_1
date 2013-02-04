f25651c36aede8557d5a4c90bae18b6655f9b57f
Yijun Gong <yg2306@columbia.edu>
2013-02-04 15:48:05 -0500

    Yijun finished the length module

--- src/data_length.py
+++ src/data_length.py
@@ -1,4 +1,4 @@
-
+   
 def compute_length(infile):
     """
     Returns the length (number of rows) of the csv file.
@@ -9,3 +9,7 @@ def compute_length(infile):
         Should be opened with 'r' permissions
     """
     # Try using the csv module 
+    import csv
+    rows = list(csv.reader(infile))
+    row_count = len(rows)
+    return row_count
